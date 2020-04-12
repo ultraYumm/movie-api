ce6c27e10e4c898a6372ad4054902281b3d6b587
Stephanie <Stephanie@Office-iMac.local>
2020-04-12 13:28:49 -0700

    added security features

--- server.js
+++ server.js
@@ -1,13 +1,67 @@
+require ('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const cors = require ('cors')
+console.log(process.env.API_TOKEN)
+const helmet = require('helmet')
+const MOVIEDATA = require('./movies-data-small.json')
+
 
 const app = express()
 
 app.use(morgan('dev'))
+app.use(cors())
+app.use(helmet())
 
-app.use((req, res) => {
-  res.send('Hello, w!')
+app.use(function validateBearerToken(req, res, next) {
+  
+  const apiToken = process.env.API_TOKEN
+  const authToken = req.get('Authorization')
+
+  if (!authToken || authToken.split(' ')[1] !== apiToken) {
+   return res.status(401).json({ error: 'Unauthorized request' })
+   }
+
+  next()
+
+
 })
+
+
+function handleGetMovie(req, res) {
+  let response = MOVIEDATA
+
+  if (req.query.genre) {
+    response = response.filter(movies =>
+      // case insensitive searching
+      movies.genre.toLowerCase().includes(req.query.genre.toLowerCase())
+    )
+  }
+  if (req.query.country) {
+    response = response.filter(movies =>
+      // case insensitive searching
+      movies.country.toLowerCase().includes(req.query.country.toLowerCase())
+    )
+  }
+
+  
+if (req.query.average_vote) {
+    let selection = req.query.average_vote
+    response = response.filter(movies =>   
+    movies.avg_vote >= selection)
+    
+  }
+
+  res.json(response)    
+}
+
+
+
+
+ app.get('/movie', handleGetMovie)
+
+
+   
 
 const PORT = 8020
 
